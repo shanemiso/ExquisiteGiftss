08dce4df1cb49973aba394098199661a212062b1
ArnavGorkhali <arnavgorkhali@gmail.com>
2024-04-20 22:04:16 +0545

    Migrations Final setup

--- database/migrations/2024_03_31_170659_create_user_payments_table.php
+++ database/migrations/2024_03_31_170659_create_user_payments_table.php
@@ -4,18 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
+    protected $fillable = ['userId', 'id', 'paymentType', 'accountNumber'];
+
     public function up()
     {
         Schema::create('user_payments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->string('accountNumber')->nullable();
+            $table->enum('paymentType', ['cash', 'card', 'wallet'])->default('cash');
+
             $table->timestamps();
+
+            $table->foreign('userId')->references('id')->on('users');
         });
     }
 
